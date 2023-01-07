72f6794168115fe91a9daec8666cbc255c1572c5
RobertLita <lita.robert@yahoo.com>
2023-01-07 23:58:16 +0200

    history page updated with leagues & matches

--- src/pages/History.tsx
+++ src/pages/History.tsx
@@ -1,11 +1,18 @@
+import { Box } from '@mantine/core'
 import React from 'react'
 import Search from '../components/search/Search'
+import Leagues from '../components/summoner-history/Leagues'
+import Match from '../components/summoner-history/Match'
 
 const History: React.FC = () => {
   return (
-    <div>
-        <Search />
-    </div>
+    <>
+      <Search />
+      <Box mt={20} ml = {20} mr = {20} display='flex' sx ={{justifyContent: 'space-evenly'}}>
+        <Leagues />
+        <Match />
+      </Box>
+    </>
   )
 }
 
